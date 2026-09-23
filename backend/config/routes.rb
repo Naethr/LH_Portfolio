0c4596e9d7200aba3c17393fc68be803c5e8509a
Naethr <xenotimedev@gmail.com>
2026-09-23 14:36:15 +0200

    Add show action to projects resource and adjust routing structure

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :projects, only: [:index]
-    end
+      resources :projects, only: [:index, :show], param: :slug
+
+        namespace :admin do
+          resource :session, only: [:show, :create, :destroy]
+        end
+      end
   end
 end
