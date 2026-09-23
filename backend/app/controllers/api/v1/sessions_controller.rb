f438b004e8ed1f145f4d3d1ecc041369f668b427
Naethr <xenotimedev@gmail.com>
2026-09-23 15:13:14 +0200

    Move admin sessions controller for user authentication into the right folder

---
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::Admin::SessionsController < ApplicationControlle
+  allow_unauthenticated_access only: :create
+
+  def create
+    user = User.authenticate_by(
+      email_address: params[:email_address],
+      password: params[:password]
+      )
+    
+    if user
+      start_new_session_for(user)
+
+      render json: {
+        user: {
+          email_address: user.email_address
+        }
+      }, status: :created
+
+    else
+      render json: {
+        error: "Invalid email or password"
+      }, status: :unauthorized
+    end
+  end
+
+  def show
+    render json: {
+      user: {
+        email_address: Current.user.email_address
+      }
+    }
+  end
+
+  def destroy
+    terminate_session
+
+    head :no_content
+  end
+end
